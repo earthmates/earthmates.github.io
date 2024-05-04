21977529cf0d7dab871d0d66504bdafb2d544dba
JacopoCeravolo <77071770+JacopoCeravolo@users.noreply.github.com>
2024-05-04 17:40:21 +0200

    removed resources

--- src/_codux/boards/resources/resources.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Resources } from '../../../components/resources/resources';
-
-export default createBoard({
-    name: 'Resources',
-    Board: () => <Resources />,
-    isSnippet: true,
-});
