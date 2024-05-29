e0088c394e68799358c62d4dc330d7f7cad9a216
Filipe Bertolucci <filipe@bertolucci.it>
2024-05-29 12:49:53 +0200

    having probleminstalling material ui here... i will make a pull request so that u take a look: try: npm install @material-ui/core

--- src/components/commonLanding/CommonLanding.tsx
+++ src/components/commonLanding/CommonLanding.tsx
@@ -1,17 +1,57 @@
 import React from "react";
-import "./commonlanding.modules.scss";
+import "./commonlanding.scss";
 import { Button } from "../button/button";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
 export interface InvestorsProps {
   role: string;
 }
 function CommonLanding({ role }: InvestorsProps) {
+  var color;
+  var secondaryColor;
   if (role === "startup") {
-    //here goes the code for startups common landing
+    //here goes the code for startups colors
+    color = "#FF8516";
+    secondaryColor = "#FFEEDF";
   } else {
-    //here goes the code for investors common landing
+    //here goes the code for investors colors
+    color = "#2F2B6F";
+    secondaryColor = "#E0DFEA";
   }
 
-  return <div></div>;
+  //here goes the page code
+  //divided into statup and investor
+  if (role === "startup") {
+    return (
+      <div className="external-container">
+        {/*first container */}
+        <div className="first-container">
+          <div className="title-container">
+            <h1 className="title"></h1>
+            <p className="paragraph"></p>
+          </div>
+          <div className="content">
+            <div id="mainImage"></div>
+            <div className="text-container">
+              <h1 className="title"></h1>
+              <p className="paragraph"></p>
+            </div>
+          </div>
+        </div>
+        {/*second container*/}
+        <div className="second-container">
+          <div className="title-container">
+            <h1 className="title"></h1>
+            <p className="paragraph"></p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <div className="external-container"></div>;
+  }
 }
 
 export default CommonLanding;
