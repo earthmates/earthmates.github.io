7071e394ea7fc91316cd79f1fc4c5245c00b9109
EarthMates <163406866+EarthMates@users.noreply.github.com>
2024-06-16 11:55:22 +0200

    Update button.tsx

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -12,7 +12,7 @@ export interface ButtonProps {
 }
 
 const defaultClick = () => {
-  window.location.href = "http://18.159.198.188:3000/register";
+  window.location.href = "https://app.earthmates.de/register";
 };
 
 export const Button: React.FC<ButtonProps> = ({
