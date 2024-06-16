e1e5e453ddca90ae9b6aa65a47a76a39863fc9b2
EarthMates <163406866+EarthMates@users.noreply.github.com>
2024-06-16 11:55:08 +0200

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -85,7 +85,7 @@ export const Header = ({ className }: HeaderProps) => {
         </div>
       )}
       <div className={styles.login}>
-        <a href="http://18.159.198.188:3000/login" className={styles["login-button"]}>
+        <a href="https://app.earthmates.de/login" className={styles["login-button"]}>
           Login
         </a>
         <Button buttonText="Get Started" />
@@ -112,7 +112,7 @@ export const Header = ({ className }: HeaderProps) => {
           </NavLink>
 
           <hr />
-          <a href="http://18.159.198.188:3000/login" className={styles["login-button"]}>
+          <a href="https://app.earthmates.de/login" className={styles["login-button"]}>
             Login
           </a>
           <Button buttonText="Get Started" />
